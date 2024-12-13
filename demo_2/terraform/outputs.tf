7b8aac832ed37ede608324ef2ec0169fcef7d5ec
stackgen-automation <automation@stackgen.com>
2024-12-13 07:36:00 +0000

    Automated commit

--- demo_2/terraform/outputs.tf
+++ demo_2/terraform/outputs.tf
@@ -1,4 +1,35 @@
 
+output "aws_rds_default_arn" {
+  value = module.stackgen_e58d9612-d9a8-4e87-8bb6-f81ad07d0cd2.arn
+  sensitive = false
+}
+
+output "aws_rds_default_connection_string" {
+  value = module.stackgen_e58d9612-d9a8-4e87-8bb6-f81ad07d0cd2.connection_string
+  sensitive = true
+}
+
+output "aws_rds_default_password" {
+  value = module.stackgen_e58d9612-d9a8-4e87-8bb6-f81ad07d0cd2.password
+  sensitive = true
+}
+
+output "aws_rds_default_port" {
+  value = module.stackgen_e58d9612-d9a8-4e87-8bb6-f81ad07d0cd2.port
+  sensitive = false
+}
+
+output "aws_rds_default_rds_endpoint" {
+  value = module.stackgen_e58d9612-d9a8-4e87-8bb6-f81ad07d0cd2.rds_endpoint
+  sensitive = false
+}
+
+output "aws_rds_default_username" {
+  value = module.stackgen_e58d9612-d9a8-4e87-8bb6-f81ad07d0cd2.username
+  sensitive = false
+}
+
+
 output "aws_dynamodb_dynamodb_table_arn" {
   value = module.stackgen_27cd3301-ab20-58cc-879a-6e1f05e79942.arn
   sensitive = false
