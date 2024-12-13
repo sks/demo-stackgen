990543bc9ff24198f6bf0b542b743b665bb79858
sks <sks@stackgen.com>
2024-12-12 23:23:16 -0800

    Automated commit (#3)
    Co-authored-by: stackgen-automation <automation@stackgen.com>

---
+++ iac/terraform/outputs.tf
@@ -0,0 +1,41 @@
+
+output "aws_rds_default_port" {
+  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.port
+  sensitive = false
+}
+
+output "aws_rds_default_rds_endpoint" {
+  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.rds_endpoint
+  sensitive = false
+}
+
+output "aws_rds_default_username" {
+  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.username
+  sensitive = false
+}
+
+output "aws_rds_default_arn" {
+  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.arn
+  sensitive = false
+}
+
+output "aws_rds_default_connection_string" {
+  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.connection_string
+  sensitive = true
+}
+
+output "aws_rds_default_password" {
+  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.password
+  sensitive = true
+}
+
+
+output "aws_iam_role_management_service_role_arn" {
+  value = module.stackgen_6b95b55c-7d59-5712-aca4-99df013b2bbe.arn
+  sensitive = false
+}
+
+output "aws_iam_role_management_service_role_name" {
+  value = module.stackgen_6b95b55c-7d59-5712-aca4-99df013b2bbe.name
+  sensitive = false
+}
