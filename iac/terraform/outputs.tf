8a7c254d8bd9b80665f7b4c7b70052bddb418078
sks <sks@stackgen.com>
2024-12-12 23:24:36 -0800

    Automated commit (#4)
    Co-authored-by: stackgen-automation <automation@stackgen.com>

--- iac/terraform/outputs.tf
+++ iac/terraform/outputs.tf
@@ -1,32 +1,32 @@
 
-output "aws_rds_default_port" {
-  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.port
-  sensitive = false
-}
-
-output "aws_rds_default_rds_endpoint" {
-  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.rds_endpoint
-  sensitive = false
-}
-
-output "aws_rds_default_username" {
-  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.username
-  sensitive = false
-}
-
 output "aws_rds_default_arn" {
-  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.arn
+  value = module.stackgen_bc72e470-6335-473e-b3ca-c0d3a9450b16.arn
   sensitive = false
 }
 
 output "aws_rds_default_connection_string" {
-  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.connection_string
+  value = module.stackgen_bc72e470-6335-473e-b3ca-c0d3a9450b16.connection_string
   sensitive = true
 }
 
 output "aws_rds_default_password" {
-  value = module.stackgen_1ada307e-7277-56ed-b5d2-957c2ae12405.password
+  value = module.stackgen_bc72e470-6335-473e-b3ca-c0d3a9450b16.password
   sensitive = true
+}
+
+output "aws_rds_default_port" {
+  value = module.stackgen_bc72e470-6335-473e-b3ca-c0d3a9450b16.port
+  sensitive = false
+}
+
+output "aws_rds_default_rds_endpoint" {
+  value = module.stackgen_bc72e470-6335-473e-b3ca-c0d3a9450b16.rds_endpoint
+  sensitive = false
+}
+
+output "aws_rds_default_username" {
+  value = module.stackgen_bc72e470-6335-473e-b3ca-c0d3a9450b16.username
+  sensitive = false
 }
 
 
