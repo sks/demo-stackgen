0a19903bdf3794559fd894ac83a8b255926b3f0f
stackgen-automation <automation@stackgen.com>
2024-12-19 02:45:26 +0000

    Automated commit

--- 9b95aaf7-a306-43a4-9659-cdd2a3a81c0c/terraform/modules/custom_asdasd/asdasd.tf
+++
@@ -1,9 +0,0 @@
-# /* Example: */
-# resource "aws_acm_certiasdasicate" "example" {
-#   domain_name       = var.domain_name  # Change this to your domain name
-#   validation_method = var.validation_method
-  
-#  lifecycle = {
-#    create_before_destroy = true
-#   }
-# }
