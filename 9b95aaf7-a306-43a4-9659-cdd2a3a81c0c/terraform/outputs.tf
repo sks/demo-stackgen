0adca6ec9c420acf71b3d930f788b4735b7ed572
sks <sks@stackgen.com>
2024-12-18 18:35:28 -0800

    Automated commit (#33)
    Co-authored-by: stackgen-automation <automation@stackgen.com>

--- 9b95aaf7-a306-43a4-9659-cdd2a3a81c0c/terraform/outputs.tf
+++ 9b95aaf7-a306-43a4-9659-cdd2a3a81c0c/terraform/outputs.tf
@@ -1,10 +1,10 @@
 
-output "aws_iam_role_studentservice_role_name" {
-  value = module.stackgen_1722699a-6e36-5f09-85c4-a852acd5604d.name
+output "aws_iam_role_studentservice_role_arn" {
+  value = module.stackgen_1722699a-6e36-5f09-85c4-a852acd5604d.arn
   sensitive = false
 }
 
-output "aws_iam_role_studentservice_role_arn" {
-  value = module.stackgen_1722699a-6e36-5f09-85c4-a852acd5604d.arn
+output "aws_iam_role_studentservice_role_name" {
+  value = module.stackgen_1722699a-6e36-5f09-85c4-a852acd5604d.name
   sensitive = false
 }
