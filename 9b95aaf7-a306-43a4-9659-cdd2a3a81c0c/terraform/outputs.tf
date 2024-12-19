a5e7b25ff42c8c26ac6b3f0095b808aebb047e16
stackgen-automation <automation@stackgen.com>
2024-12-19 01:45:57 +0000

    Automated commit

---
+++ 9b95aaf7-a306-43a4-9659-cdd2a3a81c0c/terraform/outputs.tf
@@ -0,0 +1,10 @@
+
+output "aws_iam_role_studentservice_role_name" {
+  value = module.stackgen_1722699a-6e36-5f09-85c4-a852acd5604d.name
+  sensitive = false
+}
+
+output "aws_iam_role_studentservice_role_arn" {
+  value = module.stackgen_1722699a-6e36-5f09-85c4-a852acd5604d.arn
+  sensitive = false
+}
